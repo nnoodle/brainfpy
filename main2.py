b72c303cff1cbde672b8b3356547d012cc6ced03
Noodles! <nnoodle@chiru.no>
2018-03-17 07:03:06 +0700

    OK THIS IS FINAL SRS

--- main2.py
+++ main2.py
@@ -2,16 +2,16 @@ exec("""import sys,re
 a,c,d,e,f,g,h=[],[],{},0,0,[0 for i in range(50)],sys
 for z in ''.join(filter(lambda x:x in '.,[]<>+-',re.sub('\\n','',open(h.argv[1],'r').read()))):a.append(z)
 for p,m in enumerate(''.join(a)):
-Y'[':c.append(p)
-Y']':s=c.pop();d[s]=p;d[p]=s
+Y[':c.append(p)
+Y]':s=c.pop();d[s]=p;d[p]=s
 while e<len(a):
   m=a[e]
-Y'>':f+=1
-Y'<':f-=1
-Y'+':K+=1
-Y'-':K-=1
-Y'.':h.stdout.write(chr(K))
-Y',':K=ord(h.stdin.read(1))
-Y'[' and K==0:e=d[e]
-Y']' and K!=0:e=d[e]
-  e+=1""".replace('Y','  if m==').replace('K','g[f]'))
+Y>':f+=1
+Y<':f-=1
+Y+':K+=1
+Y-':K-=1
+Y.':h.stdout.write(chr(K))
+Y,':K=ord(h.stdin.read(1))
+Y[' and K==0:e=d[e]
+Y]' and K!=0:e=d[e]
+  e+=1""".replace('Y',"  if m=='").replace('K','g[f]'))
