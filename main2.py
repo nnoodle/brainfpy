65b4a2b5b49481d28c17cc59c70d4a0735a130e6
Noodles! <nnoodle@chiru.no>
2018-03-17 19:43:13 +0700

    e v e n   s m o l l e r
    thats rite fellas its smoller

--- main2.py
+++ main2.py
@@ -1,17 +1,7 @@
 exec("""import sys,re
-a,c,d,e,f,g,h=[],[],{},0,0,[0 for i in range(50)],sys
-for z in ''.join(filter(lambda x:x in '.,[]<>+-',re.sub('\\n','',open(h.argv[1],'r').read()))):a.append(z)
-for p,m in enumerate(''.join(a)):
-Y[':c.append(p)
-Y]':s=c.pop();d[s]=p;d[p]=s
+a,c,d,e,f,g,h=[],[],{},0,0,[0 for i in range(999)],sys
+for z in ''.join(filter(lambda x:x in'.,[]<>+-',re.sub('\\n','',open(h.argv[1],'r').read()))):a.append(z)
+for p,m in enumerate(''.join(a)):A[':c.append(p)A]':s=c.pop();d[s]=p;d[p]=s
 while e<len(a):
-  m=a[e]
-Y>':f+=1
-Y<':f-=1
-Y+':K+=1
-Y-':K-=1
-Y.':h.stdout.write(chr(K))
-Y,':K=ord(h.stdin.read(1))
-Y[' and K==0:e=d[e]
-Y]' and K!=0:e=d[e]
-  e+=1""".replace('Y',"  if m=='").replace('K','g[f]'))
+  m=a[e]A>':f+=1A<':f-=1A+':g[f]+=1A-':g[f]-=1A.':h.stdout.write(chr(g[f]))A,':g[f]=ord(h.stdin.read(1))A[' and g[f]==0:e=d[e]A]' and g[f]!=0:e=d[e]
+  e+=1""".replace('A',"\n  if m=='"))
