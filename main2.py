82db37689e95d26fc8a0beaf1e06f512d2b2863c
nnoodle <nnoodle@chiru.no>
2019-04-14 09:28:14 +0700

    Properly updated main2.py
    shouldn't have had ^M and spaces

--- main2.py
+++ main2.py
@@ -1,26 +1,26 @@
 import sys
 with open(sys.argv[1]) as s:
-    r=s.read()
+	r=s.read()
 j=p=T=R=0
 t=[0]*999
 def S(fro,to):
-    global j,p,R
-    R+=j
-    if r[R]==fro:p+=1
-    elif r[R]==to:
-        if p!=0:p-=1
-        else:j=0
+	global j,p,R
+	R+=j
+	if r[R]==fro:p+=1
+	elif r[R]==to:
+		if p!=0:p-=1
+		else:j=0
 while R<len(r):
-    c=r[R]
-    if j==1:S('[',']')
-    elif j==-1:S(']','[')
-    elif c=='['and t[T]==0:j=0
-    elif c==']'and t[T]!=0:j=-1
-    else:
-        if c=='.':print(chr(t[T]),end='')
-        if c==',':t[T]=ord(sys.read(1))
-        if c=='+':t[T]+=1
-        if c=='-':t[T]-=1
-        if c=='>':T+=1
-        if c=='<':T-=1
-        R+=1
+	c=r[R]
+	if j==1:S('[',']')
+	elif j==-1:S(']','[')
+	elif c=='['and t[T]==0:j=0
+	elif c==']'and t[T]!=0:j=-1
+	else:
+		if c=='.':print(chr(t[T]),end='')
+		if c==',':t[T]=ord(sys.read(1))
+		if c=='+':t[T]+=1
+		if c=='-':t[T]-=1
+		if c=='>':T+=1
+		if c=='<':T-=1
+		R+=1
