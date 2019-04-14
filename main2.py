8131ad85404511143267c468f1d1391a353e6cc9
nnoodle <nnoodle@chiru.no>
2019-04-14 09:50:45 +0700

    Added exec n replace

--- main2.py
+++ main2.py
@@ -1,13 +1,11 @@
-import sys
-with open(sys.argv[1]) as s:
-	r=s.read()
+exec("""import sys
+with open(sys.argv[1]) as s:r=s.read()
 j=p=T=R=0
 t=[0]*999
-def S(fro,to):
-	global j,p,R
-	R+=j
-	if r[R]==fro:p+=1
-	elif r[R]==to:
+def S(P,Q):
+	global j,p,R;R+=j
+	if r[R]==P:p+=1
+	elif r[R]==Q:
 		if p!=0:p-=1
 		else:j=0
 while R<len(r):
@@ -16,11 +14,5 @@ while R<len(r):
 	elif j==-1:S(']','[')
 	elif c=='['and t[T]==0:j=0
 	elif c==']'and t[T]!=0:j=-1
-	else:
-		if c=='.':print(chr(t[T]),end='')
-		if c==',':t[T]=ord(sys.read(1))
-		if c=='+':t[T]+=1
-		if c=='-':t[T]-=1
-		if c=='>':T+=1
-		if c=='<':T-=1
-		R+=1
+	else:A.':print(chr(t[T]),end='')A,':t[T]=ord(sys.read(1))A+':t[T]+=1A-':t[T]-=1A>':T+=1A<':T-=1
+		R+=1""".replace('A',"\n		if c=='"))
